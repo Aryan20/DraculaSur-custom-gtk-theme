948b75ca24e746c03bec9dfe3c91fd010a09c789
Aryan Kaushik <34372791+Aryan20@users.noreply.github.com>
2021-12-21 23:43:04 +0530

    Fix for firefox theme and flatpak

--- lib-flatpak.sh
+++ lib-flatpak.sh
@@ -48,7 +48,7 @@ pakitheme_gtk3() {
   rm -rf "$build_dir"
   mkdir -p "$build_dir/files"
 
-  cp -a "$theme_path/gtk-3.0/"* "$build_dir/files"
+  cp -a "$theme_path/gtk-3.0/*" "$build_dir/files"
 
   mkdir -p "$build_dir/files/share/appdata"
   cat >"$build_dir/files/share/appdata/$gtk3_app_id.appdata.xml" <<EOF
